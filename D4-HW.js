c96dec8e914908f6159d89679c6716835245cbdb
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-05-26 02:57:39 +0000

    Update M3-D4

--- D4-HW.js
+++ D4-HW.js
@@ -11,70 +11,29 @@ What you have to do is:
 /*
 0) Get all the products from the API using a fetch DONE
 */
-let books = []
-let checkout = {
-    basket:[],
-    quantity:0,
-    total: 0
-}
-
 // console.log(checkout)
 // checkout.basket.push('hello')
 // console.log(checkout)
 
+/*
+1) Display the list of items available on the page using template literals `` and .forEach DONE
+2) Add a "add to cart button" DONE
+3) When the button is pressed, change the style of the item and add it to another list
+4) Add "skip" button next to each item
+5) When pressed, the button should remove from the page the item not interesting from the user
+*/
+
 window.onload = function(){
+
+    
 
     fetch("https://striveschool-api.herokuapp.com/books")
     .then(response => response.json())
 
-/*
-1) Display the list of items available on the page using template literals `` and .forEach DONE
-*/
-
-/*
-2) Add a "add to cart button" DONE
-*/
-
-/*
-3) When the button is pressed, change the style of the item and add it to another list
-*/
-
-/*
-4) Add "skip" button next to each item
-*/
-
-
-
-/*
-5) When pressed, the button should remove from the page the item not interesting from the user
-*/
-.then( items => {
-    let list = document.querySelectorAll('.list .row')
-    
-    // items.forEach(item => console.log(item.img))
-   
-    books.push(...items)
-   console.log(books)
-        items.forEach(item => {
-            let div = document.createElement('div')
-            div.classList.add('col-6','col-md-3', 'my-5')
-            div.innerHTML = `
-            <dd class="book">
-                <div class="card h-100">
-                    <img class="img-fluid" src= "${item.img}">
-                    <div class="card-body p-0 justify-content-center">
-                    <div class="card-header">${item.title}</div>
-                    <div class="lead text-center py-4"> £${item.price}</div>
-                    <a class="btn btn-light mt-auto w-100" style="border-radius:0">add to cart</a>
-                    </div>
-                </div>
-            </dd>
-                   `
-            list[0].appendChild(div)
-        })
-
-        addToCart(items)
-        return items
+.then(data=> {
+    console.log(data)
+displayBooks(data)
+        return data
 })
 
 
@@ -82,40 +41,35 @@ window.onload = function(){
 }
 
 
-const addToCart = (item) =>{
-
-let btn = document.getElementsByClassName('btn')
-let cart = document.getElementById('cart')
-let quantity = document.getElementById('quantity')
-let total = document.getElementById('total')
-
-
-    for(let i = 0; i < books.length; i++){
-        btn[i].addEventListener('click',(event) =>{
-           
-                checkout.basket.push(books[i].title + ": "+ "&#163"+ books[i].price)
-
-                checkout.total += books[i].price
-                total.innerText = checkout.total
-
-                checkout.quantity += 1
-                quantity.innerText = checkout.quantity
-           
-
-    cart.innerHTML += `
-    <dd class="book, col-12">
-            <dd class="lead p-0 justify-content-center">${books[i].title + ": "+ books[i].price}</dd>
-            </div>
-        </div>
-    </dd>
-           `
-
+function displayBooks(books){
+    const main = document.getElementById("book-page")
+    const search = document.getElementById("search-bar")
 
     
-        })
-    }
+    search.addEventListener("keyup" ,(e) => {
 
-    
+        if(e.target.value.length > 2){
+           
+     books.filter(book => book.title.includes(e.target.value))
+
+     console.log(filtered)
+     main.innerHTML = ""
+     filtered.forEach(book => {
+        main.innerHTML+= `
+            <div class="col-12 col-md-4 col-lg-3 my-3  align-items-stretch ">
+                <div class="card">
+                    <img class="img-fluid book-img" src= "${book.img}">
+                    <div class="card-body p-0 justify-content-center">
+                    <div class="card-header">${book.title}</div>
+                    <div class="lead text-center "> £${book.price}</div>
+                    <a class="btn bg-dark text-white mt-auto w-100" style="border-radius:0">add to cart</a>
+                    </div>
+                </div>
+                    `
+            })
+        }
+    })
+  
 }
 
 
